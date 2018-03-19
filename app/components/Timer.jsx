e776de946880859ff3ecd75a1a93cb3b61aab951
Robert Scully <rtscully@us.ibm.com>
2018-03-19 13:57:26 -0400

    add navigation

---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function () {
+        return(
+                <p>timer.jsx</p>
+        )
+    }
+})
+
+module.exports = Timer;
